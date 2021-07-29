ff4a7fa0ff3b24433b568cb63b7fadc1d1e635d1
viraj chaudhary <viraj.chaudhary@apexure.com>
2021-07-29 20:07:04 +0530

    add amooth scrool

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,17 +1,16 @@
 jQuery(function($) {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-      var target = $(this.hash);
-        $('html,body').stop().animate({
-          scrollTop: target.offset().top - 20
-        }, 'linear');
-  });
-    if (location.hash){
-    var id = $(location.hash);
-    }
-    $(window).on('load', function() {
-    if (location.hash){
-        $('html,body').animate({scrollTop: id.offset().top }, 'linear')
-    };
-    });
-});
- 
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        var target = $(this.hash);
+          $('html,body').stop().animate({
+            scrollTop: target.offset().top - 120
+          }, 'linear');   
+    });    
+      if (location.hash){
+      var id = $(location.hash);
+      }
+      $(window).on('load', function() {
+      if (location.hash){
+          $('html,body').animate({scrollTop: id.offset().top -120}, 'linear')
+      };
+      });
+  })
